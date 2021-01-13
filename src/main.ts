54ba8471eff16bef7de03942df1c85317d468fcb
PoissonPoisson <romain.delorme-glorieux@sfr.fr>
2021-01-13 22:11:23 +0100

    Update environment variable management

--- src/main.ts
+++ src/main.ts
@@ -13,9 +13,8 @@ if (!existsSync('./.env')) {
 
 // If environment varaibles file is found, check all values. 
 } else {
-  if (!Number(process.env.PORT) || Number(process.env.PORT) < 0 || Number(process.env.PORT) > 65535) {
-    process.env.PORT = '5000'
-  }
+  const port = Math.floor(Number(process.env.PORT));
+  process.env.PORT = (!port || port < 0 || 65535 < port) ? '5000' : String(port);
 
   if (!existsSync(process.env.RESOURCES)) {
     logger.error('Resources folder not exists : %s', process.env.RESOURCES);
@@ -23,15 +22,14 @@ if (!existsSync('./.env')) {
 
   process.env.QUIZ_NAME = process.env.QUIZ_NAME || 'Memory Quiz';
 
-  if (!Number(process.env.ROUNDS) || Number(process.env.ROUNDS) < 0) {
-    process.env.ROUNDS = '10';
-  }
+  const rounds = Math.floor(Number(process.env.ROUNDS));
+  process.env.ROUNDS = (!rounds || rounds < 0) ? '10' : String(rounds);
 
   process.env.NODE_ENV = process.env.NODE_ENV || 'develop';
 }
 
 // Try to start the server
-server.listen(Number(process.env.PORT || 5000), (error?: any) => {
+server.listen(process.env.PORT, (error?: any) => {
   if (error) {
     logger.error('Server no listening', error);
   } else {
